62ec715e3d493b9576776458030a604f4a165e76
Klaus Raphael Machado <67723423+KlausMachado@users.noreply.github.com>
2025-07-11 14:41:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3
-from database import criar_tabela, inserir_dados_iniciais
+from database import criar_tabela_alunos, inserir_dados_teste_alunos
 from crud import(
     inserindo_aluno,
     alterando_aluno,
@@ -8,7 +8,45 @@ from crud import(
     buscar_aluno_por_nome,
     buscar_por_turma
     )
-from relatorios import calcular_media_notas, exportar_relatorio_csv
+from relatorios import calcular_media_notas 
+from login import (
+    cadastrar_usuario, 
+    login_usuario, 
+    criar_tabela_usuarios, 
+    inserir_dados_teste_usuarios 
+    )
+
+#AUTENTICAÇÃO DE USUARIO
+criar_tabela_usuarios()
+inserir_dados_teste_usuarios()
+
+def autenticar_usuario():
+    while True:
+        print("\n===Sistema de Login===")
+        print("1- Cadastrar novo usuario")
+        print("2- Fazer login")
+        print("3- SAIR")
+        try:
+            opcao = int(input("Escolha uma opção: "))
+            print("Executando...")
+            if opcao in [1, 2]:
+                if opcao == 1:
+                    print("\n------Iniciando cadastro--------")
+                    cadastrar_usuario()
+                elif opcao == 2:
+                    print("\n------Login-----")
+                    if login_usuario():
+                        return True
+                elif opcao == 3:
+                    print("Encerrando sistema...")
+                    return False
+            else:
+                print("opção invalida")
+        
+        except ValueError:
+            print("\nA opção deve ser um numero.")
+            print("Tente novamente")
+
 
 #MENU:
 def menu(cursor, conn):
@@ -21,12 +59,12 @@ def menu(cursor, conn):
         print("5- Calcular média dos alunos")
         print("6- Buscar aluno")
         print("7- Burcar turma")
-        print("8- Exportar relatório para CSV")
+        print("8- Exportar relatorio geral em CSV")
         print("9- SAIR")
         print("-" * 30)
         try:
             escolha = int(input("\nQual ação deseja realizar? "))
-            if escolha in [1, 2, 3, 4, 5, 6, 7, 8]:
+            if escolha in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
                 if escolha == 1:
                     print("\nAlunos cadastrados:")
                     listar_alunos(cursor)
@@ -55,16 +93,20 @@ def menu(cursor, conn):
                     print("\nBuscar turma")
                     buscar_por_turma(cursor)
                     print("-" * 26)
-                elif escolha == 8:
-                    print("\nExportando relatório para CSV:")
-                    exportar_relatorio_csv(cursor)
-                elif escolha == 9:    
+                elif escolha == 9:
                     print("\nEncerrrando programa....")
                     break
+                
+                elif escolha == 8:
+                    print("Exportando relatorio...")
+                    exportar_relatorio_para_csv(cursor)
+                    print("-" * 26)
+                    
             else:
                 print("Opção invalida.")
         except ValueError:
-                print("Opção invalida.")
+                print("\nA escolha deve ser um numero.")
+                print("Tente novamente.")
                 
 
         continuar = input("\nDeseja realizar outra ação? (s/n)").lower()
@@ -72,9 +114,10 @@ def menu(cursor, conn):
             print("\nEncerrrando programa....")
             break
 if __name__ == "__main__":
-    with sqlite3.connect("cadastroAluno.db") as conn:
-        cursor = conn.cursor()
-        criar_tabela(cursor)
-        inserir_dados_iniciais(cursor)
-        conn.commit()
-        menu(cursor, conn)
+    if autenticar_usuario():
+        with sqlite3.connect("cadastroAluno.db") as conn:
+            cursor = conn.cursor()
+            criar_tabela_alunos(cursor)
+            inserir_dados_teste_alunos(cursor)
+            conn.commit()
+            menu(cursor, conn)
