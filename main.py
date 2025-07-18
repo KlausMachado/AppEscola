07863823a8a9d03348411093d4f77d403d8d278b
Klaus Raphael Machado <67723423+KlausMachado@users.noreply.github.com>
2025-07-18 16:09:18 -0300

    Add hash_senha main.py

--- main.py
+++ main.py
@@ -1,123 +1,69 @@
 import sqlite3
-from database import criar_tabela_alunos, inserir_dados_teste_alunos
-from crud import(
-    inserindo_aluno,
-    alterando_aluno,
-    deletar_aluno,
-    listar_alunos,
-    buscar_aluno_por_nome,
-    buscar_por_turma
-    )
-from relatorios import calcular_media_notas 
-from login import (
-    cadastrar_usuario, 
-    login_usuario, 
-    criar_tabela_usuarios, 
-    inserir_dados_teste_usuarios 
-    )
-
-#AUTENTICAÇÃO DE USUARIO
-criar_tabela_usuarios()
-inserir_dados_teste_usuarios()
-
-def autenticar_usuario():
-    while True:
-        print("\n===Sistema de Login===")
-        print("1- Cadastrar novo usuario")
-        print("2- Fazer login")
-        print("3- SAIR")
-        try:
-            opcao = int(input("Escolha uma opção: "))
-            print("Executando...")
-            if opcao in [1, 2]:
-                if opcao == 1:
-                    print("\n------Iniciando cadastro--------")
-                    cadastrar_usuario()
-                elif opcao == 2:
-                    print("\n------Login-----")
-                    if login_usuario():
-                        return True
-                elif opcao == 3:
-                    print("Encerrando sistema...")
-                    return False
-            else:
-                print("opção invalida")
-        
-        except ValueError:
-            print("\nA opção deve ser um numero.")
-            print("Tente novamente")
+from validacoes import validar_email, gerar_hash_senha
 
 
-#MENU:
-def menu(cursor, conn):
-    while True:
-        print("\nBem vindo ao arquivo da escola.")
-        print("1- Exibir lista de alunos ")
-        print("2- Inserir novo aluno")
-        print("3- Deletar aluno")
-        print("4- Alterar aluno")
-        print("5- Calcular média dos alunos")
-        print("6- Buscar aluno")
-        print("7- Burcar turma")
-        print("8- Exportar relatorio geral em CSV")
-        print("9- SAIR")
-        print("-" * 30)
-        try:
-            escolha = int(input("\nQual ação deseja realizar? "))
-            if escolha in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
-                if escolha == 1:
-                    print("\nAlunos cadastrados:")
-                    listar_alunos(cursor)
-                    print("-" * 30)
-                elif escolha == 2:
-                    print("\nInserindo novo aluno:")
-                    inserindo_aluno(cursor, conn)                            
-                    print("-" * 30)
-                elif escolha == 3:
-                    print("\nDeletar aluno:")
-                    deletar_aluno(cursor, conn)
-                    print("-" * 30)
-                elif escolha == 4:
-                    print("\nAlterar aluno:")
-                    alterando_aluno(cursor, conn)
-                    print("-" * 30)
-                elif escolha == 5:
-                    print("\nCalculando média dos alunos:")
-                    calcular_media_notas(cursor)
-                    print("-" * 35)
-                elif escolha == 6:
-                    print("\nBuscar aluno")
-                    buscar_aluno_por_nome(cursor)
-                    print("-" * 26)
-                elif escolha == 7:
-                    print("\nBuscar turma")
-                    buscar_por_turma(cursor)
-                    print("-" * 26)
-                elif escolha == 9:
-                    print("\nEncerrrando programa....")
-                    break
-                
-                elif escolha == 8:
-                    print("Exportando relatorio...")
-                    exportar_relatorio_para_csv(cursor)
-                    print("-" * 26)
-                    
-            else:
-                print("Opção invalida.")
-        except ValueError:
-                print("\nA escolha deve ser um numero.")
-                print("Tente novamente.")
-                
-
-        continuar = input("\nDeseja realizar outra ação? (s/n)").lower()
-        if continuar != 's':
-            print("\nEncerrrando programa....")
+with sqlite3.connect("usuarios.db") as conn:
+    cursor = conn.cursor()
+    
+    def conectar_banco(nome_banco="usuarios.db"):
+        return sqlite3.connect(nome_banco)
+            
+    def criar_tabela_usuarios():
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id    INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome  TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            senha TEXT NOT NULL
+        )
+        """)
+    
+    def cadastrar_usuario():
+        nome = input("Usuario: ").title().strip()
+        while True:
+            email = input("Email: ").strip()
+            
+            if not validar_email(email):
+                print("Email invalido. Digite novamente.")
+                continue
+            
+            cursor.execute("SELECT * FROM usuarios WHERE email =?", (email,))
+            if cursor.fetchone():
+                print("Este e-mail já está cadastrado. Digite outro.")
+                continue
             break
-if __name__ == "__main__":
-    if autenticar_usuario():
-        with sqlite3.connect("cadastroAluno.db") as conn:
-            cursor = conn.cursor()
-            criar_tabela_alunos(cursor)
-            inserir_dados_teste_alunos(cursor)
-            conn.commit()
-            menu(cursor, conn)
+        senha = input("Senha: ").strip()
+        senha_hash = gerar_hash_senha(senha)
+        cursor.execute("INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)", (nome, email, senha_hash))
+        conn.commit()
+        print(f"\nUsuario {nome} inserido com sucesso!")
+    
+    def login_usuario():
+        email = input("Email: ").strip()
+        senha = input("Senha: ").strip()
+        senha_hash = gerar_hash_senha(senha)
+            
+        cursor.execute("SELECT nome FROM usuarios WHERE email=? AND senha=?", (email, senha_hash))
+        resultado = cursor.fetchone()
+
+        if resultado:
+            print(f"Bem vindo(a), {resultado[0]}")
+            return True
+        else:
+            print("Usuario não encontrado")
+            return False
+            
+            
+#------dados de teste usuario-------
+    def inserir_dados_teste_usuarios():
+        dados = [
+            ("Usuario 1", "usuario1@email.com", gerar_hash_senha("123")),
+            ("Usuario 2", "usuario2@email.com", gerar_hash_senha("123")),
+            ("Usuario 3", "usuario3@email.com", gerar_hash_senha("123")),
+            ("Usuario 4", "usuario4@email.com", gerar_hash_senha("123"))
+        ]
+        for usuario in dados:
+            try:
+                cursor.execute("INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)", (usuario))
+            except sqlite3.IntegrityError:
+                continue
